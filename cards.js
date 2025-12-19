6d9f745f22a4d6c5d65bb6ccf3c6d7281535c997
Ahmed Hassan Khawaja <112771152+Ahmedhkhawaja@users.noreply.github.com>
2025-12-19 11:06:59 +0500

    Designed the cards to fit the boxes and fixed some styling

--- cards.js
+++ cards.js
@@ -1,5 +1,21 @@
-const cards = [
-  {img: '/images/d1.avf', title: 'Galaxy A07', price: 'Rs 38,500'},
-  {img: '/images/d1.avf', title: 'Galaxy A07', price: 'Rs 38,500'},
-  {img: '/images/d1.avf', title: 'Galaxy A07', price: 'Rs 38,500'},
-]
+const cardsa = [
+  { img: "/images/d1.avif", title: "Galaxy A07", price: "Rs 38,500" },
+  { img: "/images/d1.avif", title: "Galaxy A07", price: "Rs 38,500" },
+  { img: "/images/d1.avif", title: "Galaxy A07", price: "Rs 38,500" },
+];
+
+const cardsContainer = document.querySelector("#cardsa");
+
+cardsa.forEach((card) => {
+  cardsContainer.innerHTML += `
+    <div class="card">
+      <div class="image">
+        <img src="${card.img}">
+      </div>
+      <div class="text">
+        <h3>${card.title}</h3>
+        <p>${card.price}</p>
+      </div>
+    </div>
+  `;
+});
